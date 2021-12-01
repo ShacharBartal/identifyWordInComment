c221aa7ce73349d9a2bec2569f082f4241a9b0e0
ShacharBartal <shacharbartal@gmail.com>
2021-12-01 13:45:22 +0200

    cleanning

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -13,42 +13,40 @@ var (
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name:             "identifyWordAtComment",
-	Doc:              "Checks if customs words are contained in comments.",
-	Run:              run,
+	Name:     "identifyWordAtComment",
+	Doc:      "Checks if customs words are contained in comments.",
+	Run:      run,
 	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
-func run(pass *analysis.Pass) (interface{}, error){
-	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspectorI := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
 	nodeFilter := []ast.Node{
 		(*ast.CommentGroup)(nil),
 		(*ast.File)(nil),
 	}
-	
-	inspector.Preorder(nodeFilter, func(node ast.Node) {
+
+	inspectorI.Preorder(nodeFilter, func(node ast.Node) {
 		switch node.(type) {
 		case *ast.CommentGroup:
-			for _, co := range node.(*ast.CommentGroup).List {
-				for _, keyword := range defaultKeywords {
-					if strings.Contains(co.Text, keyword) {
-						pass.Reportf(node.Pos(), "word `%s` is contained", keyword)
-					}
-				}
-			}
+			checkForKeyWord(node.(*ast.CommentGroup), pass)
 		case *ast.File:
-			for _, cos := range node.(*ast.File).Comments {
-				for _, c := range cos.List {
-					for _, keyword := range defaultKeywords {
-						if strings.Contains(c.Text, keyword) {
-							pass.Reportf(c.Pos(), "word `%s` is contained", keyword)
-						}
-					}
-				}
+			for _, fComments := range node.(*ast.File).Comments {
+				checkForKeyWord(fComments, pass)
 			}
 		}
 	})
-	
+
 	return nil, nil
+}
+
+func checkForKeyWord(cg *ast.CommentGroup, pass *analysis.Pass) {
+	for _, c := range cg.List {
+		for _, keyword := range defaultKeywords {
+			if strings.Contains(c.Text, keyword) {
+				pass.Reportf(cg.Pos(), "word `%s` is contained", keyword)
+			}
+		}
+	}
 }
